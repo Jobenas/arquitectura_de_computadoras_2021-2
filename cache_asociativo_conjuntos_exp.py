679d85a3a9ce6ff3b8c0aae56842d0c67d2e7492
Jobenas <jobenas@energyatech.com>
2021-11-10 15:59:17 -0500

    clase 2021-11-10

--- cache_asociativo_conjuntos_exp.py
+++ cache_asociativo_conjuntos_exp.py
@@ -1,31 +1,44 @@
 import time
 
 cache_size = 10
+num_conjuntos = 2
 cache = [[0, 0]] * cache_size
+idxo = [0] * num_conjuntos
 total = 0
 hit = 0
 miss = 0
 
 
+# Politica de reemplazo FIFO
+# TODO: Implementar con politica de reemplazo LFU (Least Frequently Used)
 def exp(n):
     global total, hit, miss
-    idx = n % cache_size
     total += 1
-    if cache[idx][0] == n:
-        hit += 1
-        return cache[idx][1]
+    idxc = n % num_conjuntos
+    p = int(cache_size / num_conjuntos)
+    for i in range(idxc * p, idxc * p + p):
+        if cache[i][0] == n:
+            hit += 1
+            return cache[i][1]    
     miss += 1
     res = 1
+
     for i in range(n):
        res *= n
     
+    idx = (idxc * p) + idxo[idxc]
     cache[idx] = [n, res]
+
+    if idxo[idxc] == p - 1:
+        idxo[idxc] = 0
+    else:
+        idxo[idxc] += 1
 
     return res
 
 
 if __name__ == '__main__':
-    lista_n = [5000, 220, 457, 12, 323, 440, 323, 5000, 220, 440]
+    lista_n = [5000, 220, 457, 12, 323, 440, 323, 440, 220, 5000]
 
     for num in lista_n:
         inicio = time.perf_counter()
--- cache_asociativo_puro_exp.py
+++ cache_asociativo_puro_exp.py
@@ -2,24 +2,32 @@ import time
 
 cache_size = 10
 cache = [[0, 0]] * cache_size
+idx = 0
 total = 0
 hit = 0
 miss = 0
 
 
+# Politica de reemplazo FIFO
+# TODO: Implementar con politica de reemplazo LFU (Least Frequently Used)
 def exp(n):
-    global total, hit, miss
-    idx = n % cache_size
+    global total, hit, miss, idx
     total += 1
-    if cache[idx][0] == n:
-        hit += 1
-        return cache[idx][1]
+    for i in range(cache_size):
+        if cache[i][0] == n:
+            hit += 1
+            return cache[i][1]    
     miss += 1
     res = 1
+
     for i in range(n):
        res *= n
     
     cache[idx] = [n, res]
+    if idx == 9:
+        idx = 0
+    else:
+        idx += 1
 
     return res
 
